e51007188bf115d91c224ddcf3ce6f9ee6e0e382
Sumeet Mulani <sumeetm@yahoo-inc.com>
2014-02-01 03:55:36 +0530

    Script tells you bandwidth *usage* for the month. Not remaining quota. Sorry!

--- beam.sh
+++ beam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo 'Checking the remaining bandwidth quota on your Beam Fiber connection...';
+echo 'Checking the bandwidth usage for this month on your Beam Fiber connection...';
 curl -s "http://portal.beamtele.com/newportal/my_package_ccav/my_package.php" | grep '(Quota' | cut -f 1 -d '&' | awk -F '>' '{ print $3; }'
